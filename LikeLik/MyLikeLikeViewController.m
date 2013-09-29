15dcdf20a64a8decad86c000feee5d8e6af8898f
AlexDubikov <alex.dubikov@gmail.com>
2013-09-29 22:25:46 +0400

    Картинки вместо названий в «больше приложений»

--- LikeLik/MyLikeLikeViewController.m
+++ LikeLik/MyLikeLikeViewController.m
@@ -35,7 +35,7 @@
     
     //// [testflight passCheckpoint:@"MyLikeLik Open"];
     LikeLikString = @[@"Moscow LikeLik",@"Vienna LikeLik"];
-    LikeLikImage = @[@"Fist_animated_1",@"Fist_animated_2",@"Fist_animated_3",@"Fist_animated_4"];
+    LikeLikImage = @[@"icon_moscow_",@"icon_vienna_"];
     urls = @[@"https://itunes.apple.com/ru/app/likelik-moscow/id675597201?mt=8",@"https://itunes.apple.com/ru/app/vienna-likelik/id675607865?mt=8"];
     self.TableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     self.TableView.backgroundView = [InterfaceFunctions backgroundView];
@@ -69,6 +69,12 @@
     
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 88;
+}
+
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     
@@ -80,23 +86,29 @@
     
     
 
-    [cell addSubview:[InterfaceFunctions TableLabelwithText:[LikeLikString objectAtIndex:[indexPath row]] AndColor:[InterfaceFunctions corporateIdentity] AndFrame:CGRectMake((cell.frame.size.width-240.0)/2, (cell.frame.size.height-35.0)/2, 240.0, 35.0)]];
+    [cell addSubview:[InterfaceFunctions TableLabelwithText:[LikeLikString objectAtIndex:[indexPath row]] AndColor:[InterfaceFunctions corporateIdentity] AndFrame:CGRectMake((cell.frame.size.width)/3, (cell.frame.size.height)/2, 240.0, 35.0)]];
         
     
     
-    [cell addSubview:[InterfaceFunctions corporateIdentity_actb]];//actbwithColor:[indexPath row]+1]];
+    //[cell addSubview:[InterfaceFunctions corporateIdentity_actb]];//actbwithColor:[indexPath row]+1]];
     
     cell.backgroundView = [InterfaceFunctions CellBG];
     cell.selectedBackgroundView = [InterfaceFunctions SelectedCellBG];
     
     
-    UIImageView *imageview = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[LikeLikImage objectAtIndex:[indexPath row]]]];
-    [imageview setFrame:CGRectMake(15.0, 4.0, imageview.image.size.width, imageview.image.size.height)];
-    [imageview setImage:[UIImage imageNamed:@"knuckle_1"]];
-    CGPoint size =cell.center;
-    size.x = imageview.center.x;
-    imageview.center = size;
-    [cell addSubview:imageview];
+    //UIImageView *imageview = [[UIImageView alloc] initWithImage:[UIImage imageNamed:[LikeLikImage objectAtIndex:[indexPath row]]]];
+    //[imageview setFrame:CGRectMake(15.0, 4.0, imageview.image.size.width, imageview.image.size.height)];
+    //[imageview setImage:[UIImage imageNamed:@"knuckle_1"]];
+    //CGPoint size =cell.center;
+    //size.x = imageview.center.x;
+    //imageview.center = size;
+    UIImageView *preview = [[UIImageView alloc] initWithFrame:CGRectMake(5, 5, 78, 78)];
+    UIImage * image = [UIImage imageNamed:[LikeLikImage objectAtIndex:[indexPath row]]];
+    preview.image = image;
+    CALayer *layer = preview.layer;
+    layer.cornerRadius = 20;
+    preview.clipsToBounds = YES;
+    [cell addSubview:preview];
     
     return cell;
 }
